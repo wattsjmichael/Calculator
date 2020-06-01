2fae193053a4bea58f0f7bccef4bf4ba532ea222
Michael Watts <Wattsjmichael@gmail.com>
2020-06-01 14:17:12 -0700

    It will add addition, multiplication, division, subtraction
    Co-authored Teresa Roninski <trosinski89@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,23 @@
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number"));
+
 const add = function(number1, number2) {
   return number1 + number2;
 };
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number"));
+const sub = function(number1, number2) {
+  return number1 - number2;
+};
 
-alert(add(number1, number2));
+const mult = function(number1, number2) {
+  return number1* number2;
+};
+
+const div = function(number1, number2) {
+  return number1 / number2;
+};
+
+//alert(add(number1, number2));
+//alert(sub(number1, number2));
+//alert(mult(number1, number2));
+alert(div(number1, number2));
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Calculator
+
+#### Calculator, 06.01.2020
+
+#### By Michael Watts, Teresa Rosinski
+
+## Description
+
+Adds, Subtracts, Multiplies, and Divides User Intergers.
+
+## Setup
+
+Clone Directory from Github
+Open in VS Code
+Save all
+Open in your favorite html Browser
+
+### License
+
+Licensed under MIT License.
+Copyright (c) 2020 by Michael Watts and Teresa Rosinski
