71d3d2adff86d47f6116ed89499e8391b04042f7
Michael Watts <Wattsjmichael@gmail.com>
2020-06-01 13:52:15 -0700

    Creates All Initial Files

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+const add = function(number1, number2) {
+  return number1 + number2;
+};
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number"));
+
+alert(add(number1, number2));
