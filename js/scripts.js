01ac6838a1b6d7e8c623443078bd1f2557c7886a
Michael Watts <Wattsjmichael@gmail.com>
2020-06-03 14:34:59 -0700

    Add division and multiplication functions
    Co-Authored-By Chris Brauch <cmbrauch00@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -20,11 +20,11 @@ const divide = function(number1, number2) {
 
 $(document).ready(function() {
   $("form#add").submit(function(event) {
-  event.preventDefault();
-  const number1 = parseInt($("#add1").val());
-  const number2 = parseInt($("#add2").val());
-  const result = add(number1, number2);
-  $("#output").text(result);
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output1").text(result);
   });
 
   $("form#sub").submit(function(event) {
@@ -32,6 +32,22 @@ $(document).ready(function() {
     const number1 = parseInt($("#sub1").val());
     const number2 = parseInt($("#sub2").val());
     const result = subtract(number1, number2);
-    $("#output").text(result);
+    $("#output2").text(result);
     });
+
+    $("form#mult").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#mult1").val());
+    const number2 = parseInt($("#mult2").val());
+    const result = multiply(number1, number2);
+    $("#output3").text(result);
+    });
+
+    $("form#div").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#div1").val());
+    const number2 = parseInt($("#div2").val());
+    const result = divide(number1, number2);
+    $("#output4").text(result);
+  });
 });
