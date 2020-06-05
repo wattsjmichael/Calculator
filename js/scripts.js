aed851fd734f3fc0cc883a16d81b7dda1dee3782
Michael Watts <Wattsjmichael@gmail.com>
2020-06-04 21:55:04 -0700

    add radio buttons and new logic with else if statements

--- js/scripts.js
+++ js/scripts.js
@@ -19,35 +19,20 @@ const divide = function(number1, number2) {
 // Everything below this line is user interface logic:
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#calculator").submit(function() {
     event.preventDefault();
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
-    const result = add(number1, number2);
-    $("#output1").text(result);
-  });
-
-  $("form#sub").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#sub1").val());
-    const number2 = parseInt($("#sub2").val());
-    const result = subtract(number1, number2);
-    $("#output2").text(result);
-    });
-
-    $("form#mult").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#mult1").val());
-    const number2 = parseInt($("#mult2").val());
-    const result = multiply(number1, number2);
-    $("#output3").text(result);
-    });
-
-    $("form#div").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#div1").val());
-    const number2 = parseInt($("#div2").val());
-    const result = divide(number1, number2);
-    $("#output4").text(result);
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operator = $("input:radio[name=operator]:checked").val();
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
   });
 });
